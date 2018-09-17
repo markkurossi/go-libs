e504d6592219b30581e2860422fdbe1038a1de5e
Markku Rossi <mtr@iki.fi>
2018-09-18 07:11:51 +0800

    Encoding changes. Added new access methods.

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -10,6 +10,7 @@ package uuid
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
@@ -17,6 +18,10 @@ import (
 	"regexp"
 )
 
+// UUID is an Universally Unique IDentifier, as defined by RFC
+// 4122. The UUID represents the UUID in its binary encoding. The RFC
+// 4122 specifies the encoding and fields as follows.
+//
 // The fields are encoded as 16 octets, with the sizes and order of the
 // fields defined above, and with each field encoded with the Most
 // Significant Byte first (known as network byte order).  Note that the
@@ -34,7 +39,11 @@ import (
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 // |                         node (2-5)                            |
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
+//
+// If the UUID variant is `Microsoft', the first 3 elements are
+// encoded in little-endian format. This has practially no affect
+// unless you have to match ASCII and binary UUIDs in which case the
+// equality differs between variants.
 type UUID [16]byte
 
 type Variant uint8
@@ -46,14 +55,30 @@ const (
 	ReservedFuture
 )
 
+var variants = map[Variant]string{
+	ReservedNCS:    "Reserved, NCS",
+	RFC4122:        "RFC 4122",
+	Microsoft:      "Microsoft",
+	ReservedFuture: "Reserved, future",
+}
+
+func (v Variant) String() string {
+	name, ok := variants[v]
+	if ok {
+		return name
+	}
+	return fmt.Sprintf("{Variant %d}", v)
+}
+
 // Nil UUID is a special case UUID which has all bits set to zero.
 var Nil UUID
 
-var reUUID = regexp.MustCompile("^(urn:uuid:)?([[:xdigit:]]{8})-([[:xdigit:]]{4})-([[:xdigit:]]{4})-([[:xdigit:]]{4})-([[:xdigit:]]{12})$")
+// {urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6}
+var reUUID = regexp.MustCompile("^\\{?(urn:uuid:)?([[:xdigit:]]{8})-([[:xdigit:]]{4})-([[:xdigit:]]{4})-([[:xdigit:]]{4})-([[:xdigit:]]{12})\\}?$")
 
 func (id UUID) String() string {
-	return fmt.Sprintf("%0x-%0x-%0x-%0x-%0x",
-		id[0:4], id[4:6], id[6:8], id[8:10], id[10:16])
+	return fmt.Sprintf("%08x-%04x-%04x-%0x-%0x",
+		id.TimeLow(), id.TimeMid(), id.TimeHiAndVersion(), id[8:10], id[10:16])
 }
 
 func (id UUID) TimeLow() uint32 {
@@ -83,6 +108,16 @@ func (id UUID) TimeHiAndVersion() uint16 {
 	}
 }
 
+func (id UUID) Time() uint64 {
+	return (uint64(id.TimeHiAndVersion()&0x0f) << 48) |
+		uint64(id.TimeMid()<<32) |
+		uint64(id.TimeLow())
+}
+
+func (id UUID) Version() uint8 {
+	return uint8(id.TimeHiAndVersion() >> 12)
+}
+
 func (id UUID) ClkSeqHiAndVariant() (uint8, Variant) {
 	val := id[8]
 	if (val & 0x80) == 0 {
@@ -101,10 +136,16 @@ func (id UUID) ClkSeqLow() uint8 {
 	return id[9]
 }
 
+func (id UUID) ClkSeq() uint16 {
+	hi, _ := id.ClkSeqHiAndVariant()
+	return uint16(hi)<<8 | uint16(id.ClkSeqLow())
+}
+
 func (id UUID) Node() []byte {
 	return id[10:16]
 }
 
+// Set sets the UUIDs content from the raw binary data.
 func (id *UUID) Set(data []byte) {
 	copy(id[:], data)
 }
@@ -158,6 +199,46 @@ func Parse(value string) (UUID, error) {
 
 	id := UUID{}
 	copy(id[:], data)
+
+	_, variant := id.ClkSeqHiAndVariant()
+	if variant == Microsoft {
+		v32 := binary.BigEndian.Uint32(id[0:4])
+		binary.LittleEndian.PutUint32(id[0:4], v32)
+
+		v16 := binary.BigEndian.Uint16(id[4:6])
+		binary.LittleEndian.PutUint16(id[4:6], v16)
+
+		v16 = binary.BigEndian.Uint16(id[6:8])
+		binary.LittleEndian.PutUint16(id[6:8], v16)
+	}
+
+	return id, nil
+}
+
+func ParseData(data []byte) (UUID, error) {
+	if len(data) != 16 {
+		return Nil, errors.New("Invalid data length")
+	}
+
+	id := UUID{}
+	id.Set(data)
+
+	return id, nil
+}
+
+// New returns new version 4 UUID (random UUID).
+func New() (UUID, error) {
+	id := UUID{}
+	_, err := rand.Read(id[:])
+	if err != nil {
+		return Nil, err
+	}
+
+	// Variant: RFC4122
+	id[8] = 0x80 | (id[8] & 0x3f)
+
+	// Version: 4
+	id[6] = 0x40 | (id[6] & 0x0f)
 
 	return id, nil
 }
--- uuid/uuid_test.go
+++ uuid/uuid_test.go
@@ -16,6 +16,7 @@ import (
 var uuids = []string{
 	"urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
 	"00000000-0000-0000-0000-000000000000",
+	"{123e4567-e89b-12d3-a456-426655440000}",
 }
 
 func TestParseString(t *testing.T) {
@@ -35,5 +36,21 @@ func TestSet(t *testing.T) {
 
 	if uuid.Compare(id) != 0 {
 		t.Errorf("UUID.Set() failed")
+	}
+}
+
+func TestNew(t *testing.T) {
+	id, err := New()
+	if err != nil {
+		t.Fatalf("Failed to create UUID: %s\n", err)
+	}
+
+	_, variant := id.ClkSeqHiAndVariant()
+	if variant != RFC4122 {
+		t.Errorf("Invalid variant: %d vs. %d", variant, RFC4122)
+	}
+
+	if id.Version() != 4 {
+		t.Errorf("Invalid version: %d vs. 4", id.Version())
 	}
 }
